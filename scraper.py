f1103dd910d54ae8dc1aabc095d939415f98d4b7
Cristian Pup <cristian.pup@gmail.com>
2024-08-13 12:15:52 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,11 +1,17 @@
-import abbreviate
 import textwrap
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
 import re
+import nltk
+from nltk.corpus import stopwords
 
+
+def unique_list(list):
+    list_unique = np.array(list)
+    return list_unique
+   
 
 table_names = []
 seen_table_names = set()
@@ -15,14 +21,14 @@ all_fk_table = []
 all_pk_table = []
 all_enum_table = []
 
+# init_table_names = ['EBAN', 'NAST', 'EBUB']
+init_table_names = unique_list(load_tables('tables.csv'))
+print(init_table_names)
+
 
 def append_to_table_names(new_table_names):
-    # global table_names
-    # global seen_table_names
     global init_table_names
 
-    # using 'not in' gets very slow on lists,
-    # 'set()' does not maintain order :(
     for new_table_name in new_table_names:
         if new_table_name not in seen_table_names and new_table_name in init_table_names:
             seen_table_names.add(new_table_name)
@@ -34,80 +40,49 @@ def process_fk_table(fk_table, all_fk_table):
     new_table_names = []
 
     for tr in fk_table.find_all("tr", recursive=False):  # , limit=5
-        # print('8888888888888888888')
         tds = tr.find_all("td", recursive=False)
         cell_contents = []
 
         for td in tds:
-            # print('index:' + str(tr.index(td)))
-            # print(td.text.strip())
-
-            # 9 is the checktable
             if tr.index(td) == 9:
                 if td.text.strip() != '':
                     new_table_names.append(td.text.strip())
 
             cell_contents.append(td.text.strip())
-
-        # print(cell_contents)
         table_rows.append(cell_contents)
 
-    # global all_fk_table
     all_fk_table = all_fk_table + table_rows
-
-    # print(table_rows)
-    # print(new_table_names)
     append_to_table_names(new_table_names)
-    # print(table_names)
 
 
 def process_fields_table(table_name, fields_table, all_enum_table):
     new_table_names = []
 
     for tr in fields_table.find_all("tr", recursive=False):  # , limit=5
-        # print('8888888888888888888')
         cell_contents = [table_name]
         is_pk = 0
         current_field_name = ''
-
-        # # flag PK if it's a PK field
-        # if tr.has_attr('class'):
-        #     if tr['class'][
-        #         0] == 'info':  # tr['class'] is a list, info should be the only one there, and means it's a PK
-        #         is_pk = 1
 
         tds = tr.find_all("td", recursive=False)
         for td in tds:
             # 1 is the field name
             if tr.index(td) == 1:
                 current_field_name = td.text.strip()
-                # if is_pk == 1:
-                #     global all_pk_table
-                #     all_pk_table.append([table_name, td.text.strip()])
 
-            # print('index:' + str(tr.index(td)))
-
-            # 7 is the checktable
             if tr.index(td) == 7:
                 if td.text.strip() != '':
                     new_table_names.append(td.text.strip())
 
             if 'Possible values' in td.text:
-                # print ('bonus content!')
                 cell_contents.append('1')
                 process_enumerated_values(table_name, current_field_name, td, all_enum_table)
             else:
                 cell_contents.append(td.text.strip())
 
         global all_fields_table
-        # print(cell_contents)
         all_fields_table.append(cell_contents)
 
-    # print(new_table_names)
     append_to_table_names(new_table_names)
-
-    # df = pd.DataFrame(tableRows)
-    # df
 
 
 def process_enumerated_values(table_name, current_field_name, incoming_element, all_enum_table):
@@ -120,12 +95,6 @@ def process_enumerated_values(table_name, current_field_name, incoming_element, 
 
 
 def dump_scraped_data():
-    # print(all_tables_table)
-    # print(all_fields_table)
-    # print(all_fk_table)
-    # print(all_pk_table)
-    # print(all_enum_table)
-
     tables_description = pd.DataFrame(all_tables_table)
     tables_description.columns = ['Table Name', 'SAP Table Info', 'Table Description', 'LEANX Link']
     tables_description = tables_description.drop(['SAP Table Info', 'LEANX Link'], axis=1)
@@ -153,19 +122,13 @@ def main(init_table_names, all_fk_table, all_enum_table):
             URL = "https://www.leanx.eu/en/sap/table/" + table_name + ".html"
 
             print('breaker:' + str(breaker))
-            # print(URL)
 
             page = requests.get(URL)
-
             cell_contents = []
-
-            # print(tableName)
-            # print(page.text)
             cell_contents.append(table_name)
 
             soup = BeautifulSoup(page.content, "html.parser")
             main_content = soup.find(class_="main-container")
-            # print(mainContent.prettify())
 
             try:
                 table_long_name = main_content.find("h1").text
@@ -197,12 +160,6 @@ def main(init_table_names, all_fk_table, all_enum_table):
             except IndexError:
                 pass
 
-            # if len(content_table_headers) == 2:
-            #     fk_table = content_table_headers[1].findNext("tbody")
-            #     # print(fkTable.prettify())
-            #
-            #     # process_fk_table(fk_table, all_fk_table)
-
             breaker = breaker + 1
     except:
         dump_scraped_data()
@@ -217,11 +174,6 @@ def load_tables(csv_path):
     tables_list = tables['filename'].values.tolist()
 
     return tables_list
-
-
-def unique_list(list):
-    list_unique = np.array(list)
-    return list_unique
 
 
 def urlify(s):
@@ -250,15 +202,31 @@ def prepare_short_strings(csv_path):
     mapping_table['Table Description Short'] = mapping_table['Table Description Short'].apply(urlify)
     mapping_table['Table Description Short'] = mapping_table['Table Description Short'].str.lower()
 
-    mapping_table.to_csv('SAP_Tables_Columns_prepared.csv', index=None)
-    mapping_table.to_csv('SAP_Tables_Columns_prepared_2.csv', index=None)
+    return mapping_table
 
 
-# init_table_names = ['EBAN', 'NAST', 'EBUB']
-init_table_names = unique_list(load_tables('tables.csv'))
-print(init_table_names)
 
-# main(init_table_names, all_fk_table, all_enum_table)
+main(init_table_names, all_fk_table, all_enum_table)
 
-prepare_short_strings('SAP_Tables_Columns.csv')
 
+croda_mapping = prepare_short_strings('SAP_Tables_Columns.csv')
+
+croda_mapping = croda_mapping.rename(columns={'Table Name': 'SAP Table Name',
+                                              'Column Name': 'SAP Column Name',
+                                              'Table Description': 'LEANX Table Description',
+                                              'Column Description': 'LEANX Column Description',
+                                              'Column Description Short': 'CDP Column translated'
+                                              })
+
+croda_mapping['CDP Table Name raw'] = 'croda_' + croda_mapping['SAP Table Name'].str.lower() + '_raw'
+croda_mapping['CDP Table Name translated'] = 'croda_' + croda_mapping['SAP Table Name'].str.lower() + '_' + croda_mapping['Table Description Short'].str.lower()
+
+croda_mapping['CDP New Table Name'] = ''
+croda_mapping['CDP New Table Column'] = ''
+
+croda_mapping = croda_mapping[['SAP Table Name', 'SAP Column Name',
+                               'LEANX Table Description', 'LEANX Column Description',
+                               'CDP Table Name raw', 'CDP Table Name translated', 'CDP Column translated',
+                               'CDP New Table Name', 'CDP New Table Column']]
+
+croda_mapping.to_csv('SAP_Tables_Columns_prepared.csv', index=None)
